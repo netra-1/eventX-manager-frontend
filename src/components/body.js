101b51776b9a6f74ca9c42c35aa0e3d72caace8e
netra-1 <budhathoki.netra01@gmail.com>
2022-11-10 16:13:57 +0545

    read and add event type with api integration

--- src/components/body.js
+++ src/components/body.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import AdminDashboard from './manager/dashbord/dashboard';
+import AddEventType from './manager/eventTypes/add_event_type';
+import { cakeColumns, decorationColumns, eventTypeColumns, themeColumns, venueColumns } from './datatable/datatablesource';
 
 const Body = () =>{
     return(
         <>
         <Routes>
             <Route path = '/' element={<AdminDashboard/>}/>
+
+            <Route path="/event-type" element={ <AddList columns={eventTypeColumns} /> } />
+            <Route path="/event-type/new" element={ <AddEventType /> } />
         </Routes>
         </>
     )
