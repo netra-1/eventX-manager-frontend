6114132c562eedb68219c868ebe4a0d7b5b903b1
netra-1 <budhathoki.netra01@gmail.com>
2022-11-11 17:17:51 +0545

    read and add venue added

--- src/components/body.js
+++ src/components/body.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import AdminDashboard from './manager/dashbord/dashboard';
 import AddEventType from './manager/eventTypes/add_event_type';
 import { cakeColumns, decorationColumns, eventTypeColumns, themeColumns, venueColumns } from './datatable/datatablesource';
+import UpdateEventType from './manager/eventTypes/update_event_type';
+import AddVenue from './manager/venue/add_venue';
 
 const Body = () =>{
     return(
@@ -11,6 +13,11 @@ const Body = () =>{
 
             <Route path="/event-type" element={ <AddList columns={eventTypeColumns} /> } />
             <Route path="/event-type/new" element={ <AddEventType /> } />
+            <Route path="/event-type/update/:eventTypeId" element={ <UpdateEventType /> } />
+
+            <Route path="/venue" element={ <AddList columns={venueColumns} /> } />
+            <Route path="/venue/new" element={ <AddVenue /> } />
+            
         </Routes>
         </>
     )
